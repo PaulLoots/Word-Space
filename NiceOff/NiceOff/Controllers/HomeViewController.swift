8b7a5a2a5df605c922fa9331569b4ed09afcfd31
Paul Loots <170083@virtualwindow.co.za>
2019-09-28 15:22:31 +0200

    feat: Create Xcode project

---
+++ NiceOff/NiceOff/Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NiceOff
+//
+//  Created by Paul Loots on 2019/09/28.
+//  Copyright © 2019 Paul Loots. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
