37a30b0638a8dc74e411633268261536a3da323b
Paul Loots <170083@virtualwindow.co.za>
2019-10-13 18:07:04 +0200

    feat: Add Additional Monster Images

--- NiceOff/NiceOff/Models/Avatars.swift
+++ NiceOff/NiceOff/Models/Avatars.swift
@@ -21,7 +21,13 @@ class Avatar {
 }
 
 let avatars = [Avatar(icon: "purple-monster-1", background: "planet-1", colour: "Purple"),
-               Avatar(icon: "green-monster-1", background: "planet-2", colour: "Green")]
+               Avatar(icon: "green-monster-1", background: "planet-2", colour: "Green"),
+               Avatar(icon: "lightblue-monster-1", background: "planet-1", colour: "LightBlue"),
+               Avatar(icon: "blue-monster-1", background: "planet-1", colour: "Blue"),
+               Avatar(icon: "red-monster-1", background: "planet-1", colour: "Red"),
+               Avatar(icon: "orange-monster-1", background: "planet-1", colour: "Orange"),
+               Avatar(icon: "yellow-monster-1", background: "planet-1", colour: "Yellow"),
+               Avatar(icon: "pink-monster-1", background: "planet-1", colour: "Pink")]
 
 //Avatar Name
 let avatarNameFirst = ["Bubbly","Fluffy"]
