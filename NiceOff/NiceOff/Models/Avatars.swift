da2543f1fdf495ee8946c82fc879d466e7e6c8e5
Paul Loots <170083@virtualwindow.co.za>
2019-10-22 21:15:27 +0200

    feat: Bug Fixes & Improvements

--- NiceOff/NiceOff/Models/Avatars.swift
+++ NiceOff/NiceOff/Models/Avatars.swift
@@ -38,6 +38,6 @@ let avatars = [Avatar(icon: "purple-monster-1", background: "planet-1", colour: 
                Avatar(icon: "pink-monster-2", background: "planet-5", colour: "Pink")]
 
 //Avatar Name
-let avatarNameFirst = ["Bubbly","Fluffy"]
+let avatarNameFirst = ["Bubbly","Fluffy","Adorable","Charming","Cozy","Fearless","Glitter","Quirky"]
 
-let avatarNameLast = ["Clown","Lizard"]
+let avatarNameLast = ["Clown","Lizard","Catfish","Tarantula","Sunflower","Honeybee","Starlet"]
